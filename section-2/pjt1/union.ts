6785b1ba23fd2913a57c2314990caaa133fcb16d
pgd <rudeh1253@gmail.com>
2023-07-23 12:30:39 +0900

    Cutom type

--- section-2/pjt1/union.ts
+++ section-2/pjt1/union.ts
@@ -1,4 +1,7 @@
-function combine(input1: number | string, input2: number | string, resultConversion: "as-number" | "as-text") {
+type Combinable = number | string;
+type ConversionDescription = "as-number" | "as-text";
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescription) {
     let result;
     if (typeof input1 === "number" && typeof input2 === "number" && resultConversion === "as-number") {
         result = +input1 + +input2;
