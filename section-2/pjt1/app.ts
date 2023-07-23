d81a68275801157cb7a252a31b8d8086e4532ff1
pgd <rudeh1253@gmail.com>
2023-07-23 12:40:37 +0900

    Return type

---
+++ section-2/pjt1/app.ts
@@ -0,0 +1,13 @@
+function add(n1: number, n2: number): number {
+    return n1 + n2;
+}
+
+function printResult(num: number): void {
+    console.log("Result: " + num);
+}
+
+function undefinedReturnType(): undefined {
+    return;
+}
+
+printResult(add(5, 12));
