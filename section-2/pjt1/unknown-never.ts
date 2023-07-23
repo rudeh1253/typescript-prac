5fdce8bb77e8ab8e2a4bbce952e594596c5cbccd
pgd <rudeh1253@gmail.com>
2023-07-23 14:31:07 +0900

    unknown type and never type

---
+++ section-2/pjt1/unknown-never.ts
@@ -0,0 +1,14 @@
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+if (typeof userInput === "string") {
+    userName = userInput;
+}
+
+function generateError(message: string, code: number): never {
+    throw {message: message, errorCode: code};
+}
+
+const result = generateError("An error occured!", 500);
+console.log(result);
