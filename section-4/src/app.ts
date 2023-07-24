13f1537a2277a8b10d508767becd38dee81416ae
pgd <rudeh1253@gmail.com>
2023-07-24 15:03:11 +0900

    Default parameter value

--- section-4/src/app.ts
+++ section-4/src/app.ts
@@ -1,3 +1,4 @@
-const printOutput: (a: number | string) => void = output => console.log(output);
+const add = (a: number, b: number = 1): number => a + b;
+const printOutput: (a: number | string) => void = (output: number | string): void => console.log(output);
 
-printOutput("number");
+printOutput(add(15));
